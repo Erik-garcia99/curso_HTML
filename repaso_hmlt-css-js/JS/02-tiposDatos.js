6885b08177e944341eb98856ee3fc744ed2508d3
dmrx <dmrx@wtr.com>
2025-05-14 22:09:34 -0700

    repaso a JS pero mucho mas rapidoooo

--- repaso_hmlt-css-js/JS/02-tiposDatos.js
+++ repaso_hmlt-css-js/JS/02-tiposDatos.js
@@ -19,3 +19,50 @@ let name = "erik"
 let alias = 'DMRX'
 
 let emial = `drmx@githum.com`
+
+
+//dato primito son los numeros 
+
+
+let age = 37 //entero
+let heiht = 1.60 //flotnate
+
+
+//booleanos  (true - false)
+
+let isStudent = false //no es un profesor 
+
+let isTeacher = true // si, 
+
+//Undefind, la varibale se declaro peroe no se le da un valor asociado. 
+
+let undefindValue
+
+console.log(undefindValue)
+
+// NULL valor nulo 
+
+let nullValue= null //al contrario con el undefind, nosotros intencional,mente le marcamos que el valor es nulo. 
+
+
+//Symbol 
+
+/*
+    es un tipo de datos primitivo es usado como identificador. 
+    este valor es unico 
+
+ */
+
+
+let mySymbol= Symbol("mysymbol")
+
+//BigInt, es un numero entero muy grande, tanto como la memoria tenga la maquina
+
+let myBigInt= BigInt(232435435454545)
+
+//como en python typeof nos devulve el tipo de dato de la varibale 
+console.log(typeof name)
+
+
+
+
---
+++ repaso_hmlt-css-js/JS/04-Operators.js
@@ -0,0 +1,82 @@
+//operadores 
+
+/*
+que cojones sonnnn??
+no cierto se claramento que son 
+
+para realizar operaciones dependeod la funcionalidad 
+
+*/
+
+//operasdores aritmeticos 
+
+
+let a = 5
+let b = 10
+
+let suma = a + b
+
+console.log(suma) //suma
+
+/*
+JS tienes las operaciones aritmeticas tipicas de toda la vida 
+
++ suma
+- resta 
+* multiplicacion 
+/ divicion 
+
+tenemos las otras 
+
+% modulo (el modulo es el resto de la divicion)
+
+** potencias 
+
+++ es incremento (le suma 1 al valro actual mas no se si funcion como en C incremeten post o pre)
+
+-- decremento se le resta 1 al valor actual 
+
+
+*/
+
+//opreacores de asignacion 
+
+/**
+ * 
+
+= asignacion el valro litereañl 
+
+
+
+ */
+
+let myVariable = 2
+console.log(myVariable)
+//varibale se aginacion que le suma al resultado de la vairbale en este caso la repsuesta seria 4 porqueseria 2 que tiene la viarbale anteroir mas los 2 se le estan sumando es como hacer  (myVaribale = myVaribale +2)
+
+myVariable += 2 // muy parecido a C
+console.log(myVariable)
+
+//funciona de igual froma con los demas operaciones basico 
+
+myVariable -= 2
+myVariable *= 2
+myVariable /= 2
+myVariable %= 2
+myVariable **= 2
+
+
+//opreaciodes de compracion
+
+//muy parecido  C
+// > mayor que 
+// < menor que 
+// >= mayor o igual 
+// <= menor o igual 
+// == igual que, igyuladar por valor
+
+//=== igualdad de identidad, por tipo y valor 
+
+
+
+
