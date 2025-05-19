471d8d2025623c9c387be5216319aabfb5710a7f
erik garcia <73853913+Erik-garcia99@users.noreply.github.com>
2025-05-19 10:07:55 -0700

    arrays

---
+++ repaso_hmlt-css-js/JS/10-arrays.js
@@ -0,0 +1,18 @@
+// arreglos (arryas)
+//funciona como una lista, al aparecer 
+
+
+//al aprecer se declara muy parecido a la de python, este array nos permite alamacenar una serie de datos desde la misma varibale 
+let arreglo = []
+
+//podemos crear de la sigueitne manera 
+
+let myArray = new Array()
+
+arreglo = [ 1 ] //esta es la manera mejor para declarar un arreglo 
+
+//pero que hace myArray si hago lo mismo que el arreglo, loq ue hace myArray es crear un objeto de la clae Array que lo que hace es reservar espacios en memoria 
+
+console.log(arreglo)
+
+console.log(myArray)
